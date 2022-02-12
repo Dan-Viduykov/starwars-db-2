72bb6297a83132b45fb97c7f44f8c886c4a5a4a3
Danya_Vid <danilviduykov@gmail.com>
2022-02-12 18:46:02 +0500

    create new react + ts progect

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css'
+
+export const App = (): React.ReactElement => {
+    return (
+        <div className="app">
+            <header>header</header>
+            <main>main</main>
+            <footer>footer</footer>
+        </div>
+    )
+}
