085da1edda55d2c09a7a01394c55786902bb3a52
Danya_Vid <danilviduykov@gmail.com>
2022-02-23 10:39:54 +0500

    fix

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SwapiService from "../../services/SwapiService";
+import { IPerson } from "../../types";
+import Spinner from "../Spinner";
 import './ItemList.css'
 
-export const ItemList = (): React.ReactElement => {
+interface ItemListProps {
+    onItemSelected: (id: number) => void
+}
+
+export const ItemList = (props: ItemListProps): React.ReactElement => {
+    const swapiService = new SwapiService()
+
+    const [ peopleList, setPeopleList ] = useState<Partial<IPerson[]>>([])
+    const [ loading, setLoading ] = useState(true)
+
+    useEffect(() => {
+        swapiService.getAllPeople()
+            .then((people) => {
+                setPeopleList(people)
+                setLoading(false)
+            })
+    }, [])
+
+    const renderItems = (arr: IPerson[]) => {
+        return arr.map(({id, name}: IPerson) => {
+            return (
+                <li className="list-group-item"
+                    key={id}
+                    onClick={() => {props.onItemSelected(id)}}
+                    onKeyDown={() => {console.log('key down')}}
+                    role='menuitem' >
+                    {name}
+                </li>
+            )
+        })
+    }
+
+    const spinner = loading? <Spinner /> : null
+    const content = !loading? renderItems(peopleList as IPerson[]) : null
+
     return (
         <ul className="item-list list-group">
-            <li className="list-group-item">Luke Skywalker</li>
-            <li className="list-group-item">Darth Vader</li>
-            <li className="list-group-item">R2-D2</li>
+            {spinner}
+            {content}
         </ul>
     )
-}
+}   
